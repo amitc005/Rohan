1dd2ab17e27509554c471cc57100cf4c4fbaae48
eewpravin007 <eew.pravin@gmail.com>
2016-11-04 17:09:23 +0530

    added new file

---
+++ SGMPrinter/src/java/com/main/generator/IConstantManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.16000
+ */
+package com.main.generator;
+
+/**
+ *
+ * @author namita
+ */
+public interface IConstantManager {
+
+    final static String DATABASE_NAME = "db_bjpitcell";
+    //--------------------------------------------//
+    final static String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+    final static String DATABASE_URL = "jdbc:mysql://localhost:3310/offsetmanager";
+    final static String DATABASE_USERNAME = "root";
+    final static String DATABASE_PASSWORD = "root";
+    //--------------------------------------------//
+}

289e614b384d99f6eabbaf216f74282c9d07d5ce
eewpravin007 <eew.pravin@gmail.com>
2016-11-15 15:21:44 +0530

    search method added....check it

--- SGMPrinter/src/java/com/main/servlet/ClientServlet.java
+++ SGMPrinter/src/java/com/main/servlet/ClientServlet.java
@@ -5,6 +5,8 @@ import com.main.service.ClientService;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -31,8 +33,10 @@ public class ClientServlet extends HttpServlet {
                 viewRequest(request, response);
             } else if (foraction.equals(IServletConstant.ACTION_AUTOCOMPLET)) {
                 autoCompletRequest(request, response);
-            }
-        } catch (Exception e) {
+            }else if (foraction.equals(IServletConstant.ACTION_SEARCH)) {
+                search(request, response);
+            } 
+            } catch (Exception e) {
             request.getSession().setAttribute(IServletConstant.MESSAGE, e.getMessage());
             response.sendRedirect(IServletConstant.PAGE_FAILUER);
         }
@@ -142,5 +146,22 @@ public class ClientServlet extends HttpServlet {
             }
         }
         return tblClient;
+    }
+    private void search(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //// get all search parameter 
+            String city_name = request.getParameter("city_name"); 
+            
+            /// create mapper pojo fields
+            HashMap<String, String> hashMap = new HashMap<>();  
+            
+            List<TblClient> searchClient = ClientService.searchClient(hashMap);
+            
+            request.getSession().setAttribute("clientsearch", searchClient);
+            
+            response.sendRedirect(IServletConstant.PAGE_VIEW_CLIENT);
+        } catch (IOException ex) {
+            Logger.getLogger(ClientService.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
